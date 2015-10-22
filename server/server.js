97f12740342f06a28c95d7a1e649206d68becb86
Jordan Winkelman <jordanwink201@gmail.com>
2015-10-22 12:25:30 -0700

    fixed merge conflicts

--- server/server.js
+++ server/server.js
@@ -5,8 +5,16 @@
 ***/
 
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
 
+// mongoose.connect('mongodb://localhost/dbname'); // connect to mongo database
+
+// Call the middleware function and pass the app and express as parameters
+require('./config/middleware.js')(app, express);
+
 console.log('listening on port 3001');
 app.listen(3001);
+
+module.exports = app;
---
+++ server/users/userController.js
@@ -0,0 +1,32 @@
+/***
+
+  User Controller
+
+  Save or retreive the existing user to/from the database
+
+***/
+
+var User = require('./userModel.js');
+var Utils = require('../config/utility.js');
+
+module.exports = {
+
+  // GET
+  signin : function(){
+
+  },
+
+  signout : function(){
+
+  },
+
+  // POST
+  createAccount : function(){
+
+  },
+
+  updateUser : function(){
+
+  }
+  
+};
