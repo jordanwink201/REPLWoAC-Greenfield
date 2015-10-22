640b2af8ece59923b24a8d679a139cd6a1db1251
Jordan Winkelman <jordanwink201@gmail.com>
2015-10-21 22:13:21 -0700

    configured grunt files

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,14 @@ var paths = {
 
   // Client
   clientPath : 'client/',
-  styles : this.clientPath + '/styles',
-  scripts : this.clientPath + '/scripts',
+  styles : this.clientPath + 'styles/',
+  scripts : this.clientPath + 'scripts/',
 
   // Server
-  serverPath : 'server/'
+  serverPath : 'server/',
+
+  // Build
+  distPath : 'dist/'
 
 };
 
@@ -30,14 +33,68 @@ module.exports = function(grunt) {
 
     // Linting
     jshint : {
-      files : [ paths.scripts + '*.js' ]
-      // options : {
-      //   force : true,
-      //   ignores : []
-      // }
+      files : [ 'client/scripts/*.js' ],
+      options : {
+        force : true,
+        ignores : []
+      }
     },
 
+    // Testing
 
+
+    // 
+    concat : {
+      dist : {
+        src : 'client/scripts/**/*.js',
+        dest : 'client/scripts/production.js'
+      }
+    },
+
+    uglify : {
+      build : {
+        src : 'client/scripts/production.js',
+        dest : 'client/scripts/production.min.js'
+      }
+    },
+
+    sass : {
+      dist : {
+        options : {
+          style : 'compressed' // or 'expanded'
+        },
+        files : {
+          'client/styles/main.css' : 'client/styles/main.scss'
+        }
+      }
+    },
+
+    copy : { // copy 
+      files : {
+        expand: true,
+        cwd: 'client/',
+        src : ['scripts/production.min.js', 'styles/main.css', 'styles/main.css.map', 'index.html'],
+        dest : 'dist/'
+      }
+    },
+
+    watch : {
+      options : {
+        livereload : true // must include the livereload.js file
+      },
+      scripts : {
+        files : ['client/scripts/**/*.js'],
+        tasks : ['jshint', 'concat'] // add test
+      },
+      css : {
+        files : ['client/styles/**/*.scss'],
+        tasks : ['sass']
+      }
+    },
+
+    clean : {
+      build : ['client/styles/main.css.map'],
+    },
 
   });
 
@@ -47,7 +104,13 @@ module.exports = function(grunt) {
 
   ***/
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-nodemon');
 
   /***
@@ -57,12 +120,18 @@ module.exports = function(grunt) {
   ***/
 
   grunt.registerTask('serve', ['nodemon']);
-  grunt.registerTask('test', ['jshint']);
+
+  // Test
+  // grunt.registerTask('test', ['jshint']);
+
+  grunt.registerTask('watcher', ['watch']);
 
   /***
 
     Scrum Master Grunt Tasks
 
   ***/
+
+  grunt.registerTask('build', ['jshint', 'concat', 'uglify', 'sass', 'copy', 'clean']);
 
 };
