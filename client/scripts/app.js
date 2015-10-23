4e1c51db94ca125e04c51c82da5121ffb16602a7
Royce Leung <royceleung@gmail.com>
2015-10-22 19:33:52 -0700

    Added Controllers

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,27 +1,42 @@
 angular.module('crash', [
-  'ngRoute'])
+  'crash.home',
+  'crash.crashWitness',
+  'crash.crashWitnessInfo',
+  'crash.crashPhoto',
+  'crash.crashDriverSearch',
+  'crash.crashDriverInfo',
+  'crash.crashEmail',
+  'ngRoute'
+])
 .config(function($routeProvider, $httpProvider) {
 	$routeProvider
     .when('/home', {
-     templateUrl: 'scripts/modules/home/home.html'
+     templateUrl: 'scripts/modules/home/home.html',
+     controller: 'HomeController'
    })
     .when('/crashWitness', {
-     templateUrl: 'scripts/modules/crash/crashWitness/crashWitness.html'
+     templateUrl: 'scripts/modules/crash/crashWitness/crashWitness.html',
+     controller: 'CrashWitnessController'
    })
     .when('/crashWitnessInfo', {
-      templateUrl: 'scripts/modules/crash/crashWitnessInfo/crashWitnessInfo.html'
+      templateUrl: 'scripts/modules/crash/crashWitnessInfo/crashWitnessInfo.html',
+      controller: 'CrashWitnessInfoController'
     })
     .when('/crashPhoto', {
-      templateUrl: 'scripts/modules/crash/crashPhoto/crashPhoto.html'
+      templateUrl: 'scripts/modules/crash/crashPhoto/crashPhoto.html',
+      controller: 'CrashPhotoController'
     })
     .when('/crashDriverSearch', {
-      templateUrl: 'scripts/modules/crash/crashDriverSearch/crashDriverSearch.html'
+      templateUrl: 'scripts/modules/crash/crashDriverSearch/crashDriverSearch.html',
+      controller: 'CrashDriverSearchController'
     })
     .when('/crashDriverInfo', {
-      templateUrl: 'scripts/modules/crash/crashDriverInfo/crashDriverInfo.html'
+      templateUrl: 'scripts/modules/crash/crashDriverInfo/crashDriverInfo.html',
+      controller: 'CrashDriverInfoController'
     })
     .when('/crashEmail', {
-      templateUrl: 'scripts/modules/crash/crashEmail/crashEmail.html'
+      templateUrl: 'scripts/modules/crash/crashEmail/crashEmail.html',
+      controller: 'CrashEmailController'
     })
     .otherwise( {
      redirectTo: '/'
---
+++ client/scripts/modules/crash/crashDriverSearch/crashDriverSearch.js
@@ -0,0 +1,5 @@
+angular.module('crash.crashDriverSearch', [])
+
+.controller('crashDriverSearch', function() {
+
+});
