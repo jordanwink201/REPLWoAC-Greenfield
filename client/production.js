809be58c4c93c0516eb08240cf2a8b082a238480
Royce Leung <royceleung@gmail.com>
2015-10-22 17:10:19 -0700

    Setup all routes in app.js

--- client/production.js
+++ client/production.js
@@ -1,2 +1,33 @@
+angular.module('crash', [
+  'ngRoute'])
+.config(function($routeProvider, $httpProvider) {
+	$routeProvider
+    .when('/home', {
+     templateUrl: 'scripts/modules/home/home.html'
+   })
+    .when('/crashWitness', {
+     templateUrl: 'scripts/modules/crash/crashWitness/crashWitness.html'
+   })
+    .when('/crashWitnessInfo', {
+      templateUrl: 'scripts/modules/crash/crashWitnessInfo/crashWitnessInfo.html'
+    })
+    .when('/crashPhoto', {
+      templateUrl: 'scripts/modules/crash/crashPhoto/crashPhoto.html'
+    })
+    .when('/crashDriverSearch', {
+      templateUrl: 'scripts/modules/crash/crashDriverSearch/crashDriverSearch.html'
+    })
+    .when('/crashDriverInfo', {
+      templateUrl: 'scripts/modules/crash/crashDriverInfo/crashDriverInfo.html'
+    })
+    .when('/crashEmail', {
+      templateUrl: 'scripts/modules/crash/crashEmail/crashEmail.html'
+    })
+    .otherwise( {
+     redirectTo: '/'
+   });
+});
 
-var x = 1;
+
+
+
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,29 @@
 angular.module('crash', [
-'ngRoute'])
+  'ngRoute'])
 .config(function($routeProvider, $httpProvider) {
 	$routeProvider
-		.when('/crashWitness', {
-			templateUrl: 'modules/crash/crashWitness.html'
-		})
-		.when('/home', {
-			templateUrl: 'modules/home/home.html'
-		})
-		.otherwise( {
-			redirectTo: '/home'
-		})
-})
+    .when('/home', {
+     templateUrl: 'scripts/modules/home/home.html'
+   })
+    .when('/crashWitness', {
+     templateUrl: 'scripts/modules/crash/crashWitness/crashWitness.html'
+   })
+    .when('/crashWitnessInfo', {
+      templateUrl: 'scripts/modules/crash/crashWitnessInfo/crashWitnessInfo.html'
+    })
+    .when('/crashPhoto', {
+      templateUrl: 'scripts/modules/crash/crashPhoto/crashPhoto.html'
+    })
+    .when('/crashDriverSearch', {
+      templateUrl: 'scripts/modules/crash/crashDriverSearch/crashDriverSearch.html'
+    })
+    .when('/crashDriverInfo', {
+      templateUrl: 'scripts/modules/crash/crashDriverInfo/crashDriverInfo.html'
+    })
+    .when('/crashEmail', {
+      templateUrl: 'scripts/modules/crash/crashEmail/crashEmail.html'
+    })
+    .otherwise( {
+     redirectTo: '/'
+   });
+});
