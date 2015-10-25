0e8affc503d21a4b23445d0376500744b3f2e005
Jordan Winkelman <jordanwink201@gmail.com>
2015-10-24 21:59:25 -0700

    (feat) setup and tested protractor testing suite

--- specs/client/crashSpec.js
+++ specs/client/crashSpec.js
@@ -2,17 +2,18 @@ describe('Protractor Demo App', function(){
 
   // browser is a global created by Protractor, which is used for browser-level commands such as navigation with browser.get
 
-  // it('should have a title', function(){
-  //   browser.get('http://localhost:3001/#/');
-
-  //   expect(browser.getTitle()).toEqual('Super Calculator');
-  // });
+  var firstName = element(by.model('person.firstname'));
+  var lastName = element(by.model('person.lastname'));
 
   it('should add one and two', function(){
-    browser.get('http://juliemr.github.io/protractor-demo/');
+    browser.get('http://localhost:3001/#/');
 
-    element(by.css('.myclass')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
-  })
+    firstName.sendKeys('Jordan');
+    lastName.sendKeys('Winkelman');
+
+    expect(firstName.getAttribute('value')).toEqual('Jordan');
+    expect(lastName.getAttribute('value')).toEqual('Winkelman');    
+
+  });
 
 });
