29134cc4e4a31a39a4b0d45305faf2e201ef70f2
Jordan Winkelman <jordanwink201@gmail.com>
2015-10-25 17:19:11 -0700

    configured the ability to take photos

--- client/scripts/modules/crash/crashPhoto/crashPhoto.js
+++ client/scripts/modules/crash/crashPhoto/crashPhoto.js
@@ -1,47 +1,83 @@
 angular.module('crash.crashPhoto', [])
 
-.controller('CrashPhotoController', function(CrashEventObj) {
+.controller('CrashPhotoController', function($scope, CrashEventObj) {
   var self = this;
   self.images = [];
 
-  // self = {
-  //   video : null
-  // };
+  var streaming = false;
+  var width = 320;    // We will scale the photo width to this
+  var height = 0;     // This will be computed based on the input stream
 
-  // var width = 320; // We will scale the photo width to this
-  // var height = 0; // This will be computed based on the input stream
+  video = document.getElementById('video');
+  canvas = document.getElementById('canvas');
+  photo = document.getElementById('photo');
 
-  // var streaming = false;
+  // Get the Media Stream
+  navigator.getMedia = ( navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia);
+  // Fetch and start the stream
+  navigator.getMedia(
+    {
+      video: true,
+      audio: false
+    },
+    // Success Callback
+    function(stream) {
+      if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream;
+      } else {
+        var vendorURL = window.URL || window.webkitURL;
+        video.src = vendorURL.createObjectURL(stream);
+      }
+      video.play();
+    },
+    // Error Callback
+    function(err) {
+      console.log("An error occured! " + err);
+    }
+  );
 
-  // function startup() {
-  //   video = document.getElementById('video');
-  //   canvas = document.getElementById('canvas');
-  //   photo = document.getElementById('photo');
-  //   startbutton = document.getElementById('startbutton');
-  // }
+  video.addEventListener('canplay', function(ev){
+    if (!streaming) {
+      height = video.videoHeight / (video.videoWidth/width);
 
-  // console.log('self : ', self.video);
+      // Firefox currently has a bug where the height can't be read from
+      // the video, so we will make assumptions if this happens.
 
-  // navigator.getMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+      if (isNaN(height)) {
+        height = width / (4/3);
+      }
 
-  // navigator.getMedia( { video: true, audio: false },
-  //   function(stream) {
-  //     console.log('stream : ', stream);
-  //     console.log('navigator.mozGetUserMedia : ', navigator.mozGetUserMedia);
-  //     if (navigator.mozGetUserMedia) {
-  //       video.mozSrcObject = stream;
-  //     } else {
-  //       var vendorURL = window.URL || window.webkitURL;
-  //       console.log('vendorURL : ', vendorURL);
-  //       console.log('video : ', video);
-  //       video.src = vendorURL.createObjectURL(stream);
-  //     }
-  //     video.play();
-  //   },
-  //   function(err) {
-  //     console.log("An error occured! " + err);
-  //   }
-  // );
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      streaming = true;
+    }
+  }, false);
+
+  function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
+    } else {
+      clearphoto();
+    }
+  }
+
+  $scope.takePhoto = function(){
+    console.log('take photo...');
+    takepicture();
+    clearphoto();
+    
+  };
 
   /***
     save the images into the CrashEventObj.crashEvent object
